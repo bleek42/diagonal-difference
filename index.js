4f843bfb025963859f34717c55e63c36940eee56
bleek42 <brandonleek42@gmail.com>
2022-07-28 14:19:32 -0400

    started new development branch

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function diagonalDifference(arr) {
-    // Write your code here
+       // Write your code here
     let i = -1;
     let l = arr.length;
     let total = 0;
     while (i++ && l--) {
         total += arr[i][i] - arr[l][i]
     }
+    
     return Math.abs(total)
-}
+};
 
 const matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
-]
+];
 
 console.log(diagonalDifference(matrix))
